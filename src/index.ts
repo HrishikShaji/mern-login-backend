a71ce97e6325e57971a4ebcb69b31f68a6e33246
HrishikShaji <hrishikminishaji@gmail.com>
2024-01-14 21:52:05 +0530

    insertion done

--- src/index.ts
+++ src/index.ts
@@ -23,23 +23,60 @@ const app = express();
 app.use(cors(corsOptions));
 app.use(express.json());
 
-app.get("/", (req, res) => res.send("helle"));
-
 app.post("/login", (req, res) => {
-  console.log(req);
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res
+      .status(400)
+      .json({ error: "Username and password are required" });
+  }
+
+  const query = "SELECT * FROM users WHERE username = ? AND password = ?";
+  connection.query(query, [username, password], (error, results, fields) => {
+    if (error) {
+      return res.status(500).json({ error: "Internal server error" });
+    }
+
+    if (results.length > 0) {
+      return res.status(200).json({ message: "Login successful" });
+    } else {
+      return res.status(401).json({ error: "Invalid username or password" });
+    }
+  });
 });
 
-console.log(process.env.HOST);
+app.post("/signin", (req, res) => {
+  const { username, password } = req.body;
 
-connection.query(
-  "SELECT 1 + 1 AS solution",
-  function (error: any, results: any, fields: any) {
-    if (error) throw error;
-    console.log("The solution is: ", results[0].solution);
+  if (!username || !password) {
+    return res
+      .status(400)
+      .json({ error: "Username and password are required" });
   }
-);
 
-connection.end();
+  const query = "INSERT INTO users (username, password) VALUES (?, ?)";
+  console.log(username, password);
+  connection.query(query, [username, password], (error, results, fields) => {
+    if (error) {
+      console.log(error);
+      return res.status(500).json({ error: "Internal server error" });
+    }
+    console.log(results);
+    return res.status(201).json({ message: "User created successfully" });
+  });
+});
+
+process.on("SIGINT", () => {
+  connection.end((err) => {
+    if (err) {
+      console.error("Error closing MySQL connection:", err);
+    } else {
+      console.log("MySQL connection closed");
+      process.exit();
+    }
+  });
+});
 
 const port = process.env.PORT || 8082;
 
