9b2741b289877ff1f0c3ef9d95e3f55c8c48c84d
HrishikShaji <hrishikminishaji@gmail.com>
2024-01-14 16:44:39 +0530

    typescript compiler fixed

---
+++ build/index.js
@@ -0,0 +1,34 @@
+"use strict";
+console.log("hello");
+{
+    /*
+  
+  import express from "express";
+  import dotenv from "dotenv";
+  var mysql = require("mysql");
+  
+  dotenv.config();
+  
+  var connection = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+  });
+  
+  connection.connect();
+  
+  console.log("ran");
+  
+  connection.query(
+    "SELECT 1 + 1 AS solution",
+    function (error: any, results: any, fields: any) {
+      if (error) throw error;
+      console.log("The solution is: ", results[0].solution);
+    },
+  );
+  
+  connection.end();
+  
+  */
+}
